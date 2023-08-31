d3c55aa6a272cf3154da3a43d978aa63cf5bf9df
Aliaksandr <djvalentinoff@gmail.com>
2023-08-31 16:10:52 +0300

    compiling ts to js and running ts to node.js

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { sum, sub, mul, div } from './math';
+
+sum(1, 2);
+sub(1, 2);
+mul(1, 2);
+div(1, 2);
+
+console.log('sum', sum(1, 2));
+console.log('sub', sub(1, 2));
+console.log('mul', mul(1, 2));
+console.log('div', div(1, 2));
